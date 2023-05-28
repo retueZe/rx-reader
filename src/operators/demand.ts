d6c6e14a949d9f704e521d6a998aaaf06d3cc176
retueZe <84510709+retueZe@users.noreply.github.com>
2023-05-28 17:28:58 +0300

    Upgrade 'demand'

--- src/operators/demand.ts
+++ src/operators/demand.ts
@@ -1,15 +1,23 @@
 import { Failure, Success } from 'async-option'
 import { ChunkTypeId, ChunkTypeMap, SimpleOperatorIterator } from '../abstraction'
 import { areChunksEqual } from '../utils/chunk'
-import { read } from './read'
+import { peek } from './peek'
+import { skip } from './skip'
+
+export type DemandOptions = {
+    inclusive?: boolean | null
+}
 
 export function* demand<C extends ChunkTypeId = 'text', E = unknown>(
     expected: ChunkTypeMap[C],
-    errorFactory: (actual: ChunkTypeMap[C]) => E
+    errorFactory: (actual: ChunkTypeMap[C]) => E,
+    options?: Readonly<DemandOptions>
 ): SimpleOperatorIterator<ChunkTypeMap[C], E, C> {
-    const actual = yield read(expected.length, {strict: false})
+    const inclusive = options?.inclusive ?? true
+    const actual = yield peek(expected.length, {strict: false})
 
     if (!areChunksEqual(expected, actual)) return new Failure(errorFactory(actual))
+    if (inclusive) yield skip(actual.length)
 
     return new Success(actual)
 }
