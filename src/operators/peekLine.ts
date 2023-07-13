aa6dde1d53916add3497d832846e033795fdc038
retueZe <84510709+retueZe@users.noreply.github.com>
2023-07-13 18:51:50 +0300

    Add 'peekLine' operator

---
+++ src/operators/peekLine.ts
@@ -0,0 +1,53 @@
+import { ChunkTypeMap, SimpleOperatorIterator } from '../abstraction'
+import { peekWhile } from './peekWhile'
+
+/** @since v1.0.0 */
+export type PeekLineOptions = {
+    /**
+     * If `true`, retains line termination sequence.
+     * @default false
+     * @since v1.0.0
+     */
+    inclusive?: boolean | null
+}
+
+/**
+ * Peeks characters until the line ends. The condition of line breaking is CR, LF, or CRLF.
+ * @since v1.0.0
+ */
+export function* peekLine(
+    options?: Readonly<PeekLineOptions> | null
+): SimpleOperatorIterator<ChunkTypeMap['text'], never> {
+    const inclusive = options?.inclusive ?? false
+    const predicate = createLineBreakPredicate()
+    const line = yield peekWhile(predicate, {
+        inclusive: false,
+        strict: false
+    })
+
+    return inclusive
+        ? line
+        : line.endsWith('\r\n')
+            ? line.slice(0, line.length - 2)
+            : line.endsWith('\r') || line.endsWith('\n')
+                ? line.slice(0, line.length - 1)
+                : line
+}
+
+function createLineBreakPredicate(): (this: {previous: string}, char: string) => boolean {
+    const unboundPredicate = function(this: {previous: string}, char: string): boolean {
+        if (this.previous === '\n') return false
+        if (char === '\n') {
+            this.previous = '\n'
+
+            return true
+        }
+        if (this.previous === '\r') return false
+
+        this.previous = char
+
+        return true
+    }
+
+    return unboundPredicate.bind({previous: ''})
+}
